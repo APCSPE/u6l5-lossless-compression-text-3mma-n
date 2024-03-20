02a06c721f70a832fcad37d0270c2a813b59b295
3mma-n <148897839+3mma-n@users.noreply.github.com>
2024-03-20 17:19:52 +0000

    3/20

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
-#WRITE YOUR PROGRAM HERE 
-print('hello world')
+def RLE_compress(input_string):
+    current_letter = input_string[1]
+    iteration_count = 0
+    output_string = ""
+    i = 0
+    while i <= len(input_string):
+
+        while input_string[i] == current_letter:
+            iteration_count += 1
+            i + 1
+        output_string += (current_letter + str(iteration_count))
+        iteration_count = 1
+        current_letter = input_string[i]
+    return output_string
+
+
+
+
+
+print(RLE_compress("AABBBAAAABBBBBAAAAAABBBBBBB"))
+print(RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFF"))
+print(RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFFD"))
+print(RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFFDD"))
+print(RLE_compress("AACCCCBBBBBAAAAAAAXFFFFFFFFDDD"))
+print(RLE_compress("ABCDEF")) # as discussed, this one doesn't actually "compress", but it's a good test case
+print(RLE_compress("FFFFFFFFFFFFFFFFFFF"))
+print(RLE_compress("F"))
+print(RLE_compress("F????"))
+print(RLE_compress("Mmmmmmmmmm sooooo goooooood!"))
+print(RLE_compress("Booooooooooooo, hisssssssss"))
